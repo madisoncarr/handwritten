18e53e536d4554c845821f4d78b7e17491d7d533
Madison Carr <madison.carr@utexas.edu>
2019-08-09 15:45:36 -0500

    adds a trained model json file to save trained brain

--- index.js
+++ index.js
@@ -5,58 +5,37 @@ const net = new brain.NeuralNetwork({
   activation: 'sigmoid',
   hiddenLayers: [150],
   iterations: 20000,
-  learningRate: 0.05,
+  learningRate: 0.1,
 });
 
-// const sixTesting = mnist[0].set(1, 950);
-
-// const oneTesting = mnist[1].set(1, 950);
-// // sixTesting.forEach(testingObj => {
-// //   testingObj.output = 1;
-// // });
-
-// const trainingData = [...sixTesting, ...oneTesting];
-
-const set = mnist.set(200, 200);
+const set = mnist.set(800, 200);
 const trainingSet = set.training;
 const testSet = set.test;
 
-// const trainingData = [
-//   { input: [0, 0], output: [0] },
-//   { input: [0, 1], output: [1] },
-//   { input: [1, 0], output: [1] },
-//   { input: [1, 1], output: [0] },
-// ];
-
 net.train(trainingSet);
-
-// console.log('testing for 6: ', net.run(mnist[0].get(1)));
-// console.log('answer for 6: ', mnist[0].set(0, 1)[0].output);
-// console.log('testing for 6: ', net.run(mnist[0].get(2)));
-// console.log('answer for 6: ', mnist[0].set(0, 1)[0].output);
-// console.log('testing for 1: ', net.run(mnist[1].get(1)));
-// console.log('answer for 1: ', mnist[1].set(0, 1)[0].output);
-// console.log('testing for 1: ', net.run(mnist[1].get(2)));
-// console.log('answer for 1: ', mnist[1].set(0, 1)[0].output);
 
 const toNumber = arr => {
   let maxIndex = arr.indexOf(Math.max(...arr));
   return maxIndex;
 };
 
-for (let i = 0; i < 30; i++) {
-  console.log(
+let rightAnswer = 0;
+
+for (let i = 0; i < 50; i++) {
+  if (toNumber(testSet[i].output) === toNumber(net.run(testSet[i].input))) {
+    rightAnswer += 1;
+  }
+  console.error(
     `testing for ${toNumber(testSet[i].output)}: `,
     toNumber(net.run(testSet[i].input))
   );
 }
 
-// console.log(mnist[1].length); //1127
-// console.log(mnist[0].length); //1001
+console.error(`accuracy: ${(rightAnswer * 100) / 50}%`);
 
 // const digit = mnist[1].get();
-// var context = document.getElementById('myCanvas').getContext('2d');
+// const context = document.getElementById('myCanvas').getContext('2d');
 
 // mnist.draw(digit, context); // draws a '1' mnist digit in the canvas
 
-// console.log('Got to the end here');
+console.log(JSON.stringify(net.toJSON()));
